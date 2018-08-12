c809205e88dc547d9ef6e183667fb32f606141c4
Rodolfo Silveira <rodolfo360tisilveira@gmail.com>
2018-08-12 10:15:58 -0300

    javascript completo

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
 function monitoramento (){
-    const url = '../../pagina/sobre.html'
-    const botao = document.getElementById('monitor')
+    const botao1 = document.getElementById('historico')
+    const botao2 = document.getElementById('conhecimento')
     const local = document.querySelector('[wb-moderno]')
-    botao.onclick = () =>{
+    botao1.onclick = () =>{
+        const url = '../../pagina/sobre.html'
+        navegarViaAjax(url,local)
+    }
+    botao2.onclick = () => {
+        const url = '../../pagina/conhecimento.html'
         navegarViaAjax(url,local)
     }
 }
@@ -16,7 +21,6 @@ function navegarViaAjax(url, seletor){
         .then(html => seletor.innerHTML = html)
 }
 
-
 $(window).on('scroll', function() {
     if($(window).scrollTop()) {
         $('nav').addClass('transparencia');
@@ -24,3 +28,14 @@ $(window).on('scroll', function() {
         $('nav').removeClass('transparencia').css({"transition":"1s ease-out"});
     }
 });
+
+window.onhashchange = function(e){
+    const oldUrl = e.oldURL.split('#')[1]  
+    const newUrl = e.newURL.split('#')[1]
+    console.log(oldUrl,newUrl)
+    const oldMenu = document.querySelector(`.menu a[href='#${oldUrl}']`)
+    const newMenu = document.querySelector(`.menu a[href='#${newUrl}']`)
+    oldMenu && oldMenu.classList.remove('selecionado')
+    newMenu && newMenu.classList.add('selecionado')
+}
+
